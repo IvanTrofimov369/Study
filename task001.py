bc71cafb64ae30fd7ffed3a9b503f3ac9b60ced9
IvanTrofimov369 <itturkish@gmail.com>
2024-04-23 02:17:46 +0300

    init

---
+++ task001.py
@@ -0,0 +1,15 @@
+print('one', 'two', 'three') #str
+
+print(42) #int
+
+print (3.14) #float
+
+print(True) #bool
+
+x=2
+y=5
+print(x, '+', y, '=', x+y, sep = " ")
+
+print('Вася', ' пошел', '   гулять', sep = '\n')
+print(sep=("\n"))
+print('   Ж   ','  ЖЖЖ  ',' ЖЖЖЖЖ','ЖЖЖЖЖЖЖ',' HH HH  ',' ZZZZZ ',sep = '\n')
