f7bf7e3e22abb534a2c51fd4a2325b9af08b23c6
IvanTrofimov369 <itturkish@gmail.com>
2024-04-26 14:25:45 +0300

    init

--- My_Function.py
+++ My_Function.py
@@ -1,2 +1,9 @@
-def my_power(x,n):
-    return x**n
+import telebot
+bot = telebot.TeleBot("5512086789:AAHl0o7Huhh7NRo5oajlYMyTjodUvO1n-xY")
+@bot.message_handler(commands=["start"])
+def start(m, res=False):
+    bot.send_message(m.chat.id, 'Я на связи, Напиши мне')
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    bot.send_message(message.chat.id, 'Вы написали: ' + message.text)
+bot.polling(none_stop=True, interval=0)
